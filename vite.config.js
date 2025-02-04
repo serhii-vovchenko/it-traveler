12bd79e7ce5455f0819e85580d11bd5c8b8add3d
Serhii Vovchenko <serega.vovchenko@gmail.com>
2025-02-04 16:41:08 +0100

    update base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: './',
+	base: '/',
 	plugins: [vue(), vueDevTools()],
 	resolve: {
 		alias: {

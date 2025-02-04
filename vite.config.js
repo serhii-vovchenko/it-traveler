5308c415da7f63c3bca1a8f4fe7eb632c1ccc79b
Serhii Vovchenko <serega.vovchenko@gmail.com>
2025-02-04 19:30:10 +0100

    fix-deploy-rules

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig({
-	base: '/',
+	base: './',
 	plugins: [vue(), vueDevTools()],
 	resolve: {
 		alias: {
